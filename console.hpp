a25836409d79dbe450f0b19c24070cb2781654af
lukiex <166537992+0x340@users.noreply.github.com>
2026-09-04 23:43:44 +0200

    Refactor console.hpp by removing extra whitespace

--- console.hpp
+++ console.hpp
@@ -1,15 +1,11 @@
 #pragma once
-
 // <( headers )>
 #include <string>
 #include <cstdint>
-
 // <( windows )>
 #include <Windows.h>
-
 namespace console
 {
-
 	
 	// < struct >
 	struct color_t
@@ -18,7 +14,6 @@ namespace console
 		std::uint8_t g{};
 		std::uint8_t b{};
 	};
-
 	struct rect_t
 	{
 		short left{};
@@ -26,7 +21,6 @@ namespace console
 		short right{};
 		short bottom{};
 	};
-
 	struct font_t
 	{
 		std::wstring face_name{ L"Consolas" };
@@ -34,13 +28,11 @@ namespace console
 		short size_x{ 0 };
 		short size_y{ 16 };
 	};
-
 	struct cursor_info_t
 	{
 		std::uint32_t size{ 25 };
 		bool visible{ true };
 	};
-
 	struct window_info_t
 	{
 		std::string title{};
@@ -53,11 +45,9 @@ namespace console
 		COORD maximum_window_size{};
 		std::uint32_t code_page{};
 	};
-
 	// < setup >
 	void setup();
 	void shutdown();
-
 	// < other shit >
 	void set_size(short width, short height);
 	void set_window_size(short width, short height);
@@ -66,55 +56,40 @@ namespace console
 	void set_title(const std::string& title);
 	void set_title(const std::wstring& title);
 	void set_icon(HICON icon);
-
 	void set_font(const font_t& font);
 	void set_font(const std::wstring& face_name, short size_y, short weight = FW_NORMAL);
-
 	void set_transparency_alpha(std::uint8_t alpha);
-
 	void set_code_page(std::uint32_t cp);
-	void set_mode(DWORD mode);
 	void set_output_mode(DWORD mode);
 	void set_input_mode(DWORD mode);
-
 	void set_fullscreen(bool enabled);
 	void set_console_window(HWND hwnd);
 	void set_resizable(bool enabled);
 	void set_scrollable(bool enabled);
-
 	void clear();
 	void clear(color_t background);
-
 	color_t get_foreground_color();
 	color_t get_background_color();
-
 	// < colors >
 	void set_foreground_color(std::uint8_t r, std::uint8_t g, std::uint8_t b);
 	void set_background_color(std::uint8_t r, std::uint8_t g, std::uint8_t b);
-
 	void set_color(WORD attributes);
 	void set_color(color_t foreground, color_t background);
 	void set_foreground_color(color_t color);
 	void set_background_color(color_t color);
-
 	void move_window(short x, short y, short width, short height);
-
 	// < center >
 	void center_window();
 	void center_window(short width, short height);
-
 	// < flash >
 	void flash_window();
 	void flash_window(std::uint32_t count, bool invert);
-
 	// < cursor >
 	void show_cursor();
 	void hide_cursor();
-
 	void set_cursor_size(std::uint32_t size);
 	void set_cursor_visible(bool visible);
 	void set_cursor_position(short x, short y);
-
 	// < getting >
 	COORD get_cursor_position();
 	WORD get_attributes();
@@ -123,7 +98,5 @@ namespace console
 	HWND get_window_handle();
 	HANDLE get_output_handle();
 	HANDLE get_input_handle();
-
 	bool is_fullscreen();
-
 } // console
